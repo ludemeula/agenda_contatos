acc8ec3280eb620ae015b9310a69b51e1f4a797b
ludemeula <ludemeula@gmail.com>
2019-07-07 16:07:13 -0300

    Creating list view

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -7,21 +7,39 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   ContactHelper helper = ContactHelper();
+
+  List<Contact> contacts = List();
+
+  @override
+  void initState() {
+    super.initState();
+    helper.getAllContacts().then((list) {
+      setState(() {
+        contacts = list;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Contatos'),
-        backgroundColor: Colors.red,
+        backgroundColor: Color(0xff4dd0e1),
         centerTitle: true,
       ),
-      backgroundColor: Colors.white,
-      floatingActionButton: FloatingActionButton(onPressed: (){},
-      child: Icon(Icons.add),
-      backgroundColor: Colors.red,),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(Icons.add),
+          backgroundColor: Color(0xff4dd0e1)),
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          
+        },
+        itemCount: contacts.length,
+        padding: EdgeInsets.all(10),
+      ),
     );
   }
 }
